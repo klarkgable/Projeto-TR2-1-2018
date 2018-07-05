4a72a4d6e8ca912217cf91d4dc3aa36b72c43671
klarkgable <klark_94_123@hotmail.com>
2018-07-05 20:28:33 -0300

    Update Header.cpp

--- Header.cpp
+++ Header.cpp
@@ -12,20 +12,83 @@
 #include <sys/socket.h>
 
 
-///funçoes para requisiçao de header
 namespace HTTP {
 
-RequestHeader::RequestHeader() {}
+///metodo para Header
+Header::Header( std::string& palavra ) {
+	if( !palavra.empty() ) {
+		//percorrendo texto do header
+            unsigned int primeiro = 0;
+            unsigned int ultimo = palavra.find( "\r\n" );
 
-RequestHeader::RequestHeader( std::string header ) {}
+		//abstraindo a primeira linha do cabeçalho
+            PrimeiraLinha = palavra.substr( primeiro, ultimo );
 
-};
+		primeiro = ultimo + 2;
 
-///funçoes para resposta de header
-namespace HTTP {
+		while( ( ultimo = palavra.find( "\r\n", primeiro ), ultimo ) > primeiro ) {
 
-ResponseHeader::ResponseHeader() {}
+			//salva linha
+			std::string linha = palavra.substr( primeiro, ultimo-primeiro );
+			unsigned int divisao = linha.find( ":" );
 
-ResponseHeader::ResponseHeader( std::string header ) {}
+			//abstrai propriedade do cabeçalho
+			std::string propriedade = linha.substr( 0, divisao );
+			std::string valor = linha.substr( divisao + 2 );
+
+            //identidficando host
+			if( "Host" == propriedade ) {
+				divisao = valor.rfind( ":" );
+				host = valor.substr( 0, divisao );
+				if( divisao < valor.size() )
+					porta = valor.substr( divisao + 1 );
+            }
+
+
+			campos.push_back( std::make_tuple( propriedade, valor ) );
+
+			primeiro = ultimo + 2;
+		}
+
+		if( host.empty() ) {
+			unsigned int begin = PrimeiraLinha.find( "//" ) + 2;
+			unsigned int end = PrimeiraLinha.find( "/", begin );
+			std::string valor = PrimeiraLinha.substr( begin, end-begin );
+			unsigned int divisao = valor.rfind( ":" );
+			host = valor.substr( 0, divisao );
+			if( divisao < valor.size() )
+				porta = valor.substr( divisao + 1 );
+		}
+		if( '[' == host[0] ) {
+			host = host.substr( 1, host.size()-2 );
+		}
+		if( porta.empty() ) {
+			//porta padrao http
+			porta = "80";
+		}
+        //pegando corpo da mensagem
+		corpo = palavra.substr( primeiro + 2 );
+	}
+}
+
+///metodo struct para string
+std::string Header::texto(bool incluiCorpo) {
+	std::string requisicao( "" );
+
+	requisicao += PrimeiraLinha + "\r\n";
+
+	//percoorendo campos e colocando em uma string de formato da mensagem http para enviar de volta a internet
+	for( unsigned int i = 0; i < campos.size(); i++ ) {
+		requisicao += std::get<0>( campos[i] ) + ": " + std::get<1>(campos[i] ) + "\r\n";
+	}
+
+	//enter e espçamentos para mensagem
+	requisicao += "\r\n";
+	requisicao += incluiCorpo ? corpo : "<corpo>";
+
+	return requisicao;
+}
+
+
 
 };
