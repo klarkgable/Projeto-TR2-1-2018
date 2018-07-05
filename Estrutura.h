612546ad68792479aac1958aee87cf473eea0223
klarkgable <klark_94_123@hotmail.com>
2018-07-05 20:29:18 -0300

    Update Estrutura.h

--- Estrutura.h
+++ Estrutura.h
@@ -3,6 +3,9 @@
         ///Klark Gable Souza Porto  120015421
         ///Gabriel Rodrigues Diógenes Macedo  15/0126808
 
+#ifndef ESTRUTURA_H_INCLUDED
+#define ESTRUTURA_H_INCLUDED
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
@@ -10,6 +13,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <vector>
+#include <tuple>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <sys/time.h>
@@ -17,122 +21,93 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-#ifndef ESTRUTURA_H_INCLUDED
-#define ESTRUTURA_H_INCLUDED
+//para header
+#define field std::tuple< std::string, std::string >
+
 
 ///struct de mensagem para conexao(sockets)
 namespace Socket {
 
+
 typedef struct {
-	std::string message;
-	int internalConnectionID;
+	std::string mensagem;
+	int IDconexaoInterna;
 
-	std::string addr_from;
-	int port_from;
+	std::string endereco_de;
+	int porta_de;
 
-	std::string addr_to;
-	int port_to;
-} MessageData;
+	std::string endereco_para;
+	int porta_para;
+} DadosMensagem;
 
-};
+
+
+///definiçoes das estruturas e funções de conexao interna
+class conexaoInterna {
+
+    public:
+        conexaoInterna( int porta );
+        ~conexaoInterna();
+
+        void AceitaErecebe();
+        ssize_t Envio( int IDconexao, std::string mensagem );
+
+        std::vector< DadosMensagem > mensagensRecebidas;
+
+    private:
+        typedef struct {
+            //SOCKETS INTERMEDIARIOS
+                int socket;
+            struct sockaddr_in endereco;
+        } DadosConexao;
+
+        int porta;
+        //SOCKET PRINCIPAL
+            int socketPrincipal;
+
+        //criando vetor para abrir sockets caso multiplas requisiçoes
+            std::vector< DadosConexao > conexoes;
+
+        fd_set active_fd_set;
+        fd_set read_fd_set;
+        struct timeval timeout;
+
+        struct sockaddr_in endereco;
+    };
 
 
 ///definiçoes das estruturas e funções de conexao externa
-namespace Socket {
-
-class ExtListener {
+class conexaoExterna {
     public:
-        ExtListener();
-        ~ExtListener();
+        conexaoExterna(int porta);
+        ~conexaoExterna();
 
-        ssize_t Send( int connectionIDofInternal, std::string addr, int port, std::string message );
-        void ReceiveMessages();
+        ssize_t Envio( int IDconexaoDeInterna, std::string endereco, int porta, std::string mensagem );
+        void RecebeMensagens();
 
-        std::vector< MessageData > messagesReceived;
+
+        std::vector< DadosMensagem > mensagensRecebidas;
+
     private:
         typedef struct {
             int socket;
-            std::string addr;
-            int port;
-            struct sockaddr_in serv_addr;
-            int connectionIDofInternal;
-        } SocketInfo;
+            std::string endereco;
+            int porta;
+            struct sockaddr_in servidor_endereco;
+            int IDconexaoDeInterna;
+        } InfoSocket;
 
-        std::vector< SocketInfo > connections;
+        //criando vetor para abrir sockets caso multiplas requisiçoes
+            int porta
+            std::vector< InfoSocket > conexoes;
+
         fd_set active_fd_set;
         fd_set read_fd_set;
         struct timeval timeout;
     };
 
-};
-
-///definiçoes das estruturas e funções de conexao interna
-namespace Socket {
-
-class IntListener {
-
-    public:
-        IntListener( int port );
-        ~IntListener();
-
-        void AcceptAndReceive();
-        ssize_t Send( int connectionID, std::string message );
-
-        std::vector< MessageData > messagesReceived;
-    private:
-        typedef struct {
-            int socket;
-            struct sockaddr_in address;
-        } ConnectionData;
-
-        typedef enum intListenerState : short {
-            NEWBORN = 0,
-            SOCKET_CREATED = 1 << 0,
-            SOCKET_BINDED = 1 << 1,
-            INTLISTENER_STARTED = 1 << 2,
-            INTLISTENER_RUNNING = 1 << 3,
-            SHUTTING_DOWN = 1 << 4
-        } IntListenerState;
-
-        int port;
-        int socketfd;
-        short state;
-        std::vector< ConnectionData > connections;
-        fd_set active_fd_set;
-        fd_set read_fd_set;
-        struct timeval timeout;
-
-        struct sockaddr_in address;
-    };
 
 };
-
-///definição das estruturas e funções do Header
-
-    //requisiçao
-    namespace HTTP {
-
-    class RequestHeader {
-        public:
-            RequestHeader();
-            RequestHeader( std::string header );
-
-        private:
-        };
-
-    };
-
-    //resposta
-        namespace HTTP {
-
-        class ResponseHeader {
-            public:
-                ResponseHeader();
-                ResponseHeader( std::string header );
-            private:
-            };
-
-        };
 
 
 ///definição das estruturas e funções do Servidor Proxy
@@ -140,13 +115,42 @@ class IntListener {
 class ServidorProxy {
 
     public:
-        ServidorProxy( int port );
+        ServidorProxy( int porta );
         ~ServidorProxy();
         bool Loop();
 
     private:
-        Socket::IntListener il;
-        Socket::ExtListener el;
+        Socket::conexaoInterna ci;
+        Socket::conexaoExterna ce;
+};
+
+
+
+///definição das estruturas e funções do Header
+
+
+namespace HTTP {
+    //Uma´unica estrutura para cabeçalho http que identifica e separa os campos para tratar de requisiçao e resposta
+    struct Header {
+
+        //metodo para separar os campos
+        Header( std::string& palavra );
+
+        //metodo que transforma struct para string para envio na rede
+            std::string texto( bool incluiCorpo = true );
+
+        //primeira linha e campos http
+            std::string PrimeiraLinha;
+            std::vector< field > campos;
+
+        //corpo da mensagem http
+            std::string corpo;
+
+        // caminho e porta
+            std::string host;
+            std::string porta;
+    };
+
 };
 
 
