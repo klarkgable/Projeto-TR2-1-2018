07bd4b3174d8d5805eaf696b1c2bdff0927fd7b6
klarkgable <klark_94_123@hotmail.com>
2018-07-09 16:26:08 -0300

    reestruturação de algumas funções, strcuts, etc

--- Estrutura.h
+++ Estrutura.h
@@ -13,8 +13,12 @@
 #include <cstdio>
 #include <cstdlib>
 #include <vector>
+#include <memory>
 #include <tuple>
+#include <poll.h>
 #include <unistd.h>
+#include <netdb.h>
+#include <inttypes.h>
 #include <sys/socket.h>
 #include <sys/time.h>
 #include <sys/select.h>
@@ -25,111 +29,36 @@
 #define field std::tuple< std::string, std::string >
 
 
-///struct de mensagem para conexao(sockets)
-namespace Socket {
+///classe para Sockets
+class Socket {
+public:
+	Socket();
+	Socket( int Descritor );
+	~Socket();
 
+	//funçoes para atribuiçao padrao para sockets
+	bool conecta( std::string nome, std::string porta, int socketFamily = AF_INET, int socketType = SOCK_STREAM, int protocol = 0 );
+	bool percorre( std::string nome, std::string porta, int queueSize = 1, int socketFamily = AF_INET, int socketType = SOCK_STREAM, int protocol = 0 );
 
-typedef struct {
-	std::string mensagem;
-	int IDconexaoInterna;
-
-	std::string endereco_de;
-	int porta_de;
-
-	std::string endereco_para;
-	int porta_para;
-} DadosMensagem;
-
-
-
-///definiçoes das estruturas e funções de conexao interna
-class conexaoInterna {
-
-    public:
-        conexaoInterna( int porta );
-        ~conexaoInterna();
-
-        void AceitaErecebe();
-        ssize_t Envio( int IDconexao, std::string mensagem );
-
-        std::vector< DadosMensagem > mensagensRecebidas;
-
-    private:
-        typedef struct {
-            //SOCKETS INTERMEDIARIOS
-                int socket;
-            struct sockaddr_in endereco;
-        } DadosConexao;
-
-        int porta;
-        //SOCKET PRINCIPAL
-            int socketPrincipal;
-
-        //criando vetor para abrir sockets caso multiplas requisiçoes
-            std::vector< DadosConexao > conexoes;
-
-        fd_set active_fd_set;
-        fd_set read_fd_set;
-        struct timeval timeout;
-
-        struct sockaddr_in endereco;
-    };
-
-
-///definiçoes das estruturas e funções de conexao externa
-class conexaoExterna {
-    public:
-        conexaoExterna(int porta);
-        ~conexaoExterna();
-
-        ssize_t Envio( int IDconexaoDeInterna, std::string endereco, int porta, std::string mensagem );
-        void RecebeMensagens();
-
-
-        std::vector< DadosMensagem > mensagensRecebidas;
-
-    private:
-        typedef struct {
-            int socket;
-            std::string endereco;
-            int porta;
-            struct sockaddr_in servidor_endereco;
-            int IDconexaoDeInterna;
-        } InfoSocket;
-
-        //criando vetor para abrir sockets caso multiplas requisiçoes
-            int porta
-            std::vector< InfoSocket > conexoes;
-
-        fd_set active_fd_set;
-        fd_set read_fd_set;
-        struct timeval timeout;
-    };
-
-
+	bool Valido();
+	int getDescritor();
+	unsigned int getFamily();
+	uint16_t getPorta();
+	uint32_t getEndereco();
+	std::string getStringPorta();
+	std::string getStringEndereco();
+	
+	bool operator==( Socket &rhs );
+private:
+	bool valido;
+	int Descritor;
+	struct sockaddr_in endereco;
 };
-
-
-///definição das estruturas e funções do Servidor Proxy
-
-class ServidorProxy {
-
-    public:
-        ServidorProxy( int porta );
-        ~ServidorProxy();
-        bool Loop();
-
-    private:
-        Socket::conexaoInterna ci;
-        Socket::conexaoExterna ce;
-};
-
-
-
-///definição das estruturas e funções do Header
 
 
 namespace HTTP {
+///definição das estruturas e funções do Header
+
     //Uma´unica estrutura para cabeçalho http que identifica e separa os campos para tratar de requisiçao e resposta
     struct Header {
 
@@ -152,6 +81,78 @@ namespace HTTP {
     };
 
 };
+
+
+namespace Inspetor {
+
+///definiçoes das estruturas e funções de conexao interna
+class conexaoInterna {
+
+    public:
+        conexaoInterna( int porta );
+        ~conexaoInterna();
+	typedef std::tuple< std::weak_ptr< Socket >, HTTP::Header > Requisicao;
+
+
+	bool aceitaConexoes();
+	bool recebeRequisicoes();
+	void fechaSocket( int Descritor );
+ssize_t enviaResposta( std::weak_ptr< Socket > recebendoSocket, HTTP::Header resposta );
+
+	std::vector< Requisicao > requisicoesRecebidas;	
+
+    private:
+	Socket escutandoSocket;
+	std::vector< std::shared_ptr< Socket > > socketsConectados;
+
+    };
+
+
+///definiçoes das estruturas e funções de conexao externa
+class conexaoExterna {
+    public:
+        conexaoExterna(int porta);
+	conexaoExterna();
+        ~conexaoExterna();
+	typedef std::tuple< std::weak_ptr< Socket >, HTTP::Header > Resposta;
+
+	ssize_t enviaRequisicao( std::weak_ptr< Socket > requisitandoSocket, HTTP::Header requisicao );
+	bool recebeRespostas();
+
+	std::vector< Resposta > respostasRecebidas;
+	
+    private:
+        typedef std::tuple< std::shared_ptr< Socket >,std::weak_ptr< Socket > > parSocket;
+	int acharParSocket( std::weak_ptr< Socket > s_w_ptr );
+	int acharParSocket( std::shared_ptr< Socket > s_s_ptr );
+	void arrumarSockets();
+
+	std::vector< parSocket > socketsCriados;
+    };
+
+
+///definição das estruturas e funções do Servidor Proxy
+
+class ServidorProxy {
+
+    public:
+        ServidorProxy( int porta );
+        ~ServidorProxy();
+        bool Loop();
+
+	bool continuando;
+	std::vector< conexaoInterna::Requisicao > &requisicoesRecebidas;
+	std::vector< conexaoInterna::Requisicao > requisicoesEnvio;
+	std::vector< conexaoExterna::Resposta > &respostasRecebidas;
+	std::vector< conexaoExterna::Resposta > respostasEnvio;
+
+    private:
+        conexaoInterna ci;
+        conexaoExterna ce;
+  };
+
+
+};//fechando namespace Inspetor
 
 
 #endif // ESTRUTURA_H_INCLUDED
