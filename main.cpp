8f04c249add46e5cf9fb45a5f8458da56d017925
The_Programmer <garodima@gmail.com>
2018-07-10 01:16:15 -0300

    Nova identação main.cpp

--- main.cpp
+++ main.cpp
@@ -11,35 +11,32 @@
 #include <sys/socket.h>
 #include "Estrutura.h"
 
-
 #define DEFAULT_PORT 8228
 
-int main( int argc, char* argv[] ) {
-
+int main(int argc, char* argv[]){
 	int porta = DEFAULT_PORT;
-	if( 3 == argc ) {
-		if( std::string( argv[1] ) == "-p" ) {
-			try {
-				porta = std::stoi( std::string( argv[2] ) );
-			} catch( std::exception e ) {
-				printf( "\nPorta invalida.\n" );
+	if(argc == 3) {
+		if(std::string(argv[1]) == "-p"){
+			try{
+				porta = std::stoi(std::string(argv[2]));
+			}catch(std::exception e) {
+				printf("\nPorta invalida.\n");
 				return -1;
 			}
-		} else {
-			printf( "\nEntrada nao reconhecida.\nOpçoes validas:\n[-p <port>]\Escolha outra porta diferente da padrão %d\n", porta );
+		}else{
+			printf("\nEntrada nao reconhecida.\nOpçoes validas:\n[-p <port>]\\Escolha outra porta diferente da padrão %d\n", porta);
 			return -1;
 		}
-	} else if( 1 != argc ) {
-		printf( "\nNumero de argumentos invalido.\n" );
+	}else if(argc != 1){
+		printf("\nNumero de argumentos invalido.\n");
 		return -1;
 	}
 
-	
-	printf( "\nIniciando aracne na porta# %d\n", porta );
+	printf("\nIniciando aracne na porta# %d\n", porta);
 
-	Inspetor::ServidorProxy teste1( porta );
+	Inspetor::ServidorProxy teste1(porta);
 
-	while( teste1.Loop() ) {};
+	while(teste1.Loop()) {};
 
 	return 0;
 }
