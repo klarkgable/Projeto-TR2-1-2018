b1ea36f7454e52885989f5b72ba66bc483e90752
klarkgable <klark_94_123@hotmail.com>
2018-07-11 03:27:13 -0300

    comentando para documentar

--- Servidor.cpp
+++ Servidor.cpp
@@ -18,20 +18,28 @@ ServidorProxy::ServidorProxy( int porta ) : continuando(true), requisicoesRecebi
 
 ServidorProxy::~ServidorProxy() {}
 
+//loop de funcionamento do servidor, esperando interação com navegador para requisiçoes e respostas
 bool ServidorProxy::Loop() {
 	
 	continuando &= ci.aceitaConexoes();
 	continuando &= ci.recebeRequisicoes();
 
-	// Autoriza requests
+	
+//Gerando requisiçoes de dentro da máquina para fora
+	// Autoriza requisiçoes
 	if( requisicoesRecebidas.size() > 0 ) std::swap( requisicoesRecebidas, requisicoesEnvio );
 
-	// Envia requests
+	// Envia requisições
 	if( requisicoesEnvio.size() > 0 ) {
-		printf( "\nEnviando %lu requisicoes.\n", (long int) requisicoesEnvio.size() );
+		if( requisicoesEnvio.size() == 1 )		
+			printf( "\nEnviando %lu requisicao.\n", (long int) requisicoesEnvio.size() );
+		else
+			printf( "\nEnviando %lu requisicoes.\n", (long int) requisicoesEnvio.size() );
+
 		for( auto it = requisicoesEnvio.begin(); it != requisicoesEnvio.end(); it++ ) {
 			printf( "\nEnviando para %s:%s...\n%s\n\n", std::get<1>(*it).host.c_str(), std::get<1>(*it).porta.c_str(), std::get<1>(*it).texto(false).c_str() );
-			if( -1 == ce.enviaRequisicao( std::get<0>(*it), std::get<1>(*it) ) ) {
+		//caso haja falha no envio da rquisição			
+		if( -1 == ce.enviaRequisicao( std::get<0>(*it), std::get<1>(*it) ) ) {
 				fprintf( stderr, "\nNao pode completar envio de dados. Fechando... Tente novamente.\n" );
 				ci.fechaSocket( std::get<0>(*it).lock()->getDescritor() );
 			}
@@ -41,12 +49,19 @@ bool ServidorProxy::Loop() {
 
 	continuando &= ce.recebeRespostas();
 
-	// Autoriza requests
+	
+//Respota de fora da máquina para dentro da maquina
+	// Autoriza requisiçoes
 	if( respostasRecebidas.size() > 0 ) std::swap( respostasRecebidas, respostasEnvio );
 
-	// Envia requests
+	// Envia requisições
 	if( respostasEnvio.size() > 0 ) {
-		printf( "\nEnviando %lu respostas.\n", (long int) respostasEnvio.size() );
+		if( respostasEnvio.size() ==1 )		
+			printf( "\nEnviando %lu resposta.\n", (long int) respostasEnvio.size() );
+		else 
+			printf( "\nEnviando %lu respostas.\n", (long int) respostasEnvio.size() );
+
+
 		for( auto it = respostasEnvio.begin(); it != respostasEnvio.end(); it++ ) {
 			printf( "\nEnviando...\n%s\n\n", std::get<1>(*it).texto(false).c_str() );
 			if( -1 == ci.enviaResposta( std::get<0>(*it), std::get<1>(*it) ) ) {
